4ed1f76e37ee823faf42c3ac5438211eb63f39de
Stean <stean.ngugi@student.moringaschool.com>
2025-03-29 14:49:17 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
